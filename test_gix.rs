fb148644d8bbff65eea04a5f2ff83acce5474e97
Derek Marshall <derek.marshall@gmail.com>
2026-08-03 12:55:11 -0400

    chore: remove accidental test_gix.rs

--- test_gix.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let repo = gix::open("/Users/drek/src/spyc.worktrees/agy-mcp-support").unwrap();
-    let common_dir = std::fs::canonicalize(repo.common_dir()).unwrap();
-    let main_repo = gix::open(&common_dir).unwrap();
-    println!("{:?}", main_repo.work_dir().unwrap());
-}
